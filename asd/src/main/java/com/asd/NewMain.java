d07c3c26aaaa6d9c6ae2e61abc9c81f2ea98c9ee
viha <viha@192.168.1.238>
2022-02-21 22:37:27 +0700

    create a main class

--- asd/src/main/java/com/asd/NewMain.java
+++ asd/src/main/java/com/asd/NewMain.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class NewMain {
-
+//asdasdasdasd
     public static void main(String[] args) {
         ArrayList<Integer> al = new ArrayList<Integer>();
         LinkedList<Integer> ll = new LinkedList<Integer>();
